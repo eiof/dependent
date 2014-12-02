b6f092b6fc19ddbb1a3d09989437ec39c9208caf
Matt Elliott <matt.elliott@oberd.com>
2014-12-02 13:14:44 -0600

    generate starting values from balance settings

--- public/models/player.js
+++ public/models/player.js
@@ -2,23 +2,28 @@
 
 define(function (require) {
 
+  var _ = require('underscore');
   var Backbone = require('backbone');
   var RandomUser = require('../resources/random_user');
+  var balance = require('../resources/balance');
 
   var Player = Backbone.Model.extend({
     defaults: {
       name: 'John Smith',
       gender: 'male',
-      age: 21,
-      confidence: Math.floor((Math.random() * 200)),
-      hope: Math.floor((Math.random() * 200)),
-      sanity: Math.floor((Math.random() * 200)),
-      satiation: Math.floor((Math.random() * 200)),
-      thirst: Math.floor((Math.random() * 200)),
-      vigor: Math.floor((Math.random() * 200))
+      //age: 21,
+      confidence: balance.mentality.confidence.min,
+      hope: balance.mentality.hope.min,
+      sanity: balance.mentality.sanity.min,
+      satiation: balance.physicality.satiation.min,
+      hydration: balance.physicality.hydration.min,
+      vigor: balance.physicality.vigor.min
     },
 
     initialize: function () {
+      this.randomlyGenerateStartingMentality();
+      this.randomlyGenerateStartingPhysicality();
+
       var randomUser = new RandomUser();
       var _self = this;
       randomUser.fetch().then(function (response) {
@@ -30,7 +35,46 @@ define(function (require) {
         });
         window.socket.emit('new player', { player: _self.toJSON() });
       });
+    },
+
+    randomlyGenerateStartingMentality: function () {
+      var total = balance.mentality.startTotal;
+      var mentalityAttributes = ['confidence', 'hope', 'sanity'];
+
+      _.each(mentalityAttributes, function (attribute, index) {
+        var value = 0;
+        if ((mentalityAttributes.length - 1) === index) {
+          value = total;
+        } else {
+          value = this.randomlyGenerateStartingSubsetAttribute('mentality', attribute);
+          total -= value;
+        }
+        this.set(attribute, value);
+      }, this);
+    },
+
+    randomlyGenerateStartingPhysicality: function () {
+      var total = balance.physicality.startTotal;
+      var physicalityAttributes = ['satiation', 'hydration', 'vigor'];
+
+      _.each(physicalityAttributes, function (attribute, index) {
+        var value = 0;
+        if ((physicalityAttributes.length - 1) === index) {
+          value = total;
+        } else {
+          value = this.randomlyGenerateStartingSubsetAttribute('physicality', attribute);
+          total -= value;
+        }
+        this.set(attribute, value);
+      }, this);
+    },
+
+    randomlyGenerateStartingSubsetAttribute: function (subset, attribute) {
+      var minValue = balance[subset][attribute].startMin;
+      var maxValue = balance[subset][attribute].startMax;
+      return Math.floor((Math.random() * (maxValue - minValue)) + minValue);
     }
+
   });
 
   return Player;
--- public/resources/balance.js
+++ public/resources/balance.js
@@ -1,57 +1,56 @@
 define(function (require) {
 
-  return {
-    balance: {
-      ablities: {
-        carryWeight: {
-          min: 25,
-          max: 75,
-          multiplier: 0.2 // Mutliplied against vigor
-        }
+  var balance = {
+    ablities: {
+      carryWeight: {
+        min: 25,
+        max: 75,
+        multiplier: 0.2 // Mutliplied against vigor
+      }
+    },
+    mentality: {
+      startTotal: 300, // All mentality starting values must add up to this number for fairness between new players
+      confidence: {
+        min: 0,
+        max: 200,
+        startMin: 50,
+        startMax: 150
       },
-      mentality: {
-        startTotal: 300, // All mentality starting values must add up to this number for fairness between new players
-        confidence: {
-          min: 0,
-          max: 200,
-          startMin: 50,
-          startMax: 150
-        },
-        hope: {
-          min: 0,
-          max: 200,
-          startMin: 50,
-          startMax: 150
-        },
-        sanity: {
-          min: 0,
-          max: 200,
-          startMin: 50,
-          startMax: 150
-        }
+      hope: {
+        min: 0,
+        max: 200,
+        startMin: 50,
+        startMax: 150
       },
-      physicality: {
-        startTotal: 300,
-        hydration: {
-          min: 0,
-          max: 200,
-          startMin: 50,
-          startMax: 150
-        },
-        satiation: {
-          min: 0,
-          max: 200,
-          startMin: 50,
-          startMax: 150
-        },
-        vigor: {
-          min: 0,
-          max: 200,
-          startMin: 50,
-          startMax: 150
-        }
+      sanity: {
+        min: 0,
+        max: 200,
+        startMin: 50,
+        startMax: 150
+      }
+    },
+    physicality: {
+      startTotal: 300,
+      hydration: {
+        min: 0,
+        max: 200,
+        startMin: 50,
+        startMax: 150
+      },
+      satiation: {
+        min: 0,
+        max: 200,
+        startMin: 50,
+        startMax: 150
+      },
+      vigor: {
+        min: 0,
+        max: 200,
+        startMin: 50,
+        startMax: 150
       }
     }
   };
+  return balance;
 
 });
